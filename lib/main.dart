7873f09d735332ddef89dafb0f755390e457ec41
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-05-09 15:23:41 +0300

    add main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,52 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:moneymateapp/core/routing/app_routing.dart';
 import 'package:moneymateapp/firebase_options.dart';
 
-void main() async{
-    WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'MoneyMate',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-     
-    return MaterialApp.router(
-       debugShowCheckedModeBanner: false,
-        title: 'MOnyMate',
-        routerConfig: AppRouting.router,
-      
-    
-    );
+      routerConfig: AppRouting.router,
     );
   }
 }
-
