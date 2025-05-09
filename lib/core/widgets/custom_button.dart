d8bb6c638872f0780638e090a073d5f82113ac04
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-05-09 12:06:08 +0300

    add custom button

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -5,6 +5,10 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        
+      ),
+    );
   }
 }
