8865e1cc0703c545f1c004fb2041c1e82d6ea94b
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-05-09 11:24:08 +0300

    add firebase to project and change his name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:moneymateapp/firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MoneyMate',
       theme: ThemeData(
         // This is the theme of your application.
         //
