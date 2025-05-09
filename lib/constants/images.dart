efbd8aa62ab4b0cd0bb175f3f2e7be820899bacb
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-05-09 11:52:36 +0300

    add images for project

---
+++ lib/constants/images.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesHistorybottomitemblue
+  /// assets/images/historybottomitemblue.svg
+  static const String imagesHistorybottomitemblue = "assets/images/historybottomitemblue.svg";
+
+  /// Assets for imagesHistorybottomitemgrey
+  /// assets/images/historybottomitemgrey.svg
+  static const String imagesHistorybottomitemgrey = "assets/images/historybottomitemgrey.svg";
+
+  /// Assets for imagesOneprofile
+  /// assets/images/oneprofile.png
+  static const String imagesOneprofile = "assets/images/oneprofile.png";
+
+  /// Assets for imagesOvervirebottomitemblue
+  /// assets/images/overvirebottomitemblue.svg
+  static const String imagesOvervirebottomitemblue = "assets/images/overvirebottomitemblue.svg";
+
+  /// Assets for imagesOvervirebottomitemgrey
+  /// assets/images/overvirebottomitemgrey.svg
+  static const String imagesOvervirebottomitemgrey = "assets/images/overvirebottomitemgrey.svg";
+
+  /// Assets for imagesPhotoinonboarding
+  /// assets/images/photoinonboarding.png
+  static const String imagesPhotoinonboarding = "assets/images/photoinonboarding.png";
+
+  /// Assets for imagesReportsbottomitemblue
+  /// assets/images/reportsbottomitemblue.svg
+  static const String imagesReportsbottomitemblue = "assets/images/reportsbottomitemblue.svg";
+
+  /// Assets for imagesReportsbottomitemgrey
+  /// assets/images/reportsbottomitemgrey.svg
+  static const String imagesReportsbottomitemgrey = "assets/images/reportsbottomitemgrey.svg";
+
+  /// Assets for imagesSendemail
+  /// assets/images/sendemail.png
+  static const String imagesSendemail = "assets/images/sendemail.png";
+
+  /// Assets for imagesUserbottomitemblue
+  /// assets/images/userbottomitemblue.svg
+  static const String imagesUserbottomitemblue = "assets/images/userbottomitemblue.svg";
+
+  /// Assets for imagesUserbottomitemgrey
+  /// assets/images/userbottomitemgrey.svg
+  static const String imagesUserbottomitemgrey = "assets/images/userbottomitemgrey.svg";
+}
+
